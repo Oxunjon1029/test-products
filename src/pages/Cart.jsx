4fc32bb73bc39e0c9a5627475b9545638b4c39e0
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-09-06 11:43:56 +0500

    changed the title of the website

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -32,27 +32,42 @@ const Cart = () => {
       <Typography variant='h4' sx={{ textAlign: 'center' }} gutterBottom>
         Shopping Cart
       </Typography>
-      <Grid container spacing={2}>
-        {products?.map((product) => (
-          <Grid item xs={12} sm={6} md={4} key={product.id}>
-            <Card raised sx={{ height: '870px' }}>
-              <Carousel>
-                {product?.images?.map((imageUrl, index) => (
-                  <div key={imageUrl}>
-                    <img src={imageUrl} alt={`${product.title}-${index}`} />
-                  </div>
-                ))}
-              </Carousel>
-              <CardContent>
-                <Typography variant='h6'>{product.title}</Typography>
-                <Typography variant='body2' color='textSecondary'>
-                  {product.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      {products?.length === 0 && (
+        <Box
+          sx={{
+            height: '80vh',
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+          <Typography sx={{ fontSize: '50px' }}>
+            There are no products yet
+          </Typography>
+        </Box>
+      )}
+      {products?.length > 0 && (
+        <Grid container spacing={2}>
+          {products?.map((product) => (
+            <Grid item xs={12} sm={6} md={4} key={product.id}>
+              <Card raised sx={{ height: '870px' }}>
+                <Carousel>
+                  {product?.images?.map((imageUrl, index) => (
+                    <div key={imageUrl}>
+                      <img src={imageUrl} alt={`${product.title}-${index}`} />
+                    </div>
+                  ))}
+                </Carousel>
+                <CardContent>
+                  <Typography variant='h6'>{product.title}</Typography>
+                  <Typography variant='body2' color='textSecondary'>
+                    {product.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
       <Box
         sx={{
           display: 'flex',
